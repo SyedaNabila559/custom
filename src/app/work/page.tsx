34c2cddd9ba0a5f597d0ef20007aebee4ed747a4
Syeda Nabila <161962167+SyedaNabila559@users.noreply.github.com>
2024-12-04 12:05:39 -0800

    Update page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -11,7 +11,7 @@ export default function Work() {
                 <h1>work.</h1>
             </div>
             <div className="workPara">
-                <p>Nabila is a skilled front-end developer who brings creativity and precision to every project. His expertise in HTML, CSS, TypeScript, and Tailwind CSS enables him to craft engaging, user-friendly interfaces that look great on any device. Recently, he built a multi-page website using Next.js, React components, and custom CSS to ensure responsive design. Nofil has also developed a resume builder, demonstrating his versatility and problem-solving skills in web development. With a commitment to modern design and efficient code, he strives to deliver top-notch web solutions that align with the latest industry standards.</p>
+                <p>Nabila is a skilled front-end developer who brings creativity and precision to every project. His expertise in HTML, CSS, TypeScript, and Tailwind CSS enables him to craft engaging, user-friendly interfaces that look great on any device. Recently, he built a multi-page website using Next.js, React components, and custom CSS to ensure responsive design. Nabila has also developed a resume builder, demonstrating his versatility and problem-solving skills in web development. With a commitment to modern design and efficient code, he strives to deliver top-notch web solutions that align with the latest industry standards.</p>
             </div>
         </div>
         <div className="work-2">
